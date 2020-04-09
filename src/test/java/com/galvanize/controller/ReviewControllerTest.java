3160219d37f62c4bfe25460a9dc05d2c33d72b0e
Sami Akhtar <44879596+Akhtar071988@users.noreply.github.com>
2020-04-08 22:27:05 -0400

    modified the getAllReviews controller method

--- src/test/java/com/galvanize/controller/ReviewControllerTest.java
+++ src/test/java/com/galvanize/controller/ReviewControllerTest.java
@@ -53,8 +53,9 @@ public class ReviewControllerTest {
         ArrayList<Review> review = new ArrayList<>();
         review.add(expected);
         when(reviewService.getAllReviews()).thenReturn(review);
-        mvc.perform(get("/api/reviews"))
+        mvc.perform(get("/api/reviews?title=star trek&apiKey=e5a1689e"))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].rating").value(expected.getRating()))
                 .andExpect(jsonPath("$[0].reviewId").value(expected.getReviewId()));
     }
 
